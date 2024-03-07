01c2917a2595e344f14ded54eb829ea90fb138c0
A14BBS <noob1488@icloud.com>
2024-03-08 00:44:16 +0300

    Delete python_exls/GLAZBoba directory

--- python_exls/GLAZBoba/GB.py
+++
@@ -1,8 +0,0 @@
-from openpyxl import load_workbook
-
-book = load_workbook(filename= 'C:/tit/tit_test.xlsx')
-
-sheet = book['Project Timeline']
-
-for i in range(1,20):
-    print(sheet['C' + str(i)].value)
